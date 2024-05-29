82128690b2a82a467471e09a7cb2691b5c11678b
Your Name <you@example.com>
2024-05-29 15:13:32 +0530

    init

---
+++ src/app/services/jwt.ts
@@ -0,0 +1,15 @@
+import { User } from "@prisma/client";
+import { prismaClient } from "../../clients/db";
+import JWT from "jsonwebtoken";
+const JWT_SECRET = "$uper@1234";
+class JWTService {
+  public static generateTokenUser(user: User) {
+    const payload = {
+      id: user?.id,
+      email: user?.email,
+    };
+    const token = JWT.sign(payload, JWT_SECRET);
+    return token;
+  }
+}
+export default JWTService;
---
+++ src/app/user/types.ts
@@ -0,0 +1,9 @@
+export const types = `#graphql
+type user{
+id :ID!
+firstName:String!
+lastName:String
+email:String!
+prifileImageUrl:String
+}
+`;
